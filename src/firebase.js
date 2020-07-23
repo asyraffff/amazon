e126efc1e33571c83ce09b33df6ac9c4ef6da061
AMIRUL ASYRAF <58986949+asyraf-labs@users.noreply.github.com>
2020-07-23 09:55:05 +0800

    delete the importan config firebase stuff

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyBnQQ7XJdz1Uq0XHShiicFAoyVBF0srC0c",
-    authDomain: "clone-react-amazom-969fe.firebaseapp.com",
-    databaseURL: "https://clone-react-amazom-969fe.firebaseio.com",
-    projectId: "clone-react-amazom-969fe",
-    storageBucket: "clone-react-amazom-969fe.appspot.com",
-    messagingSenderId: "949626908079",
-    appId: "1:949626908079:web:6d4b19e2f78c42ae8ad35a",
-    measurementId: "G-P529TQGWK9"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 });
 
 const db = firebaseApp.firestore();
